5480a76b6f31a3bae7f8798fc161f66ad237714a
Kanu2518 <abhishekvamja2518@gmail.com>
2023-04-25 19:49:53 +0530

    Added user profile feed api

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin , BaseUserManager
-
-
+from django.conf import settings
 class UserProfileManager(BaseUserManager):
-    """Manager for user profile"""
+    """
+    Manager for user profile
+    """
 
     def create_user(self,email,name,password=None):
-        """Create a new user profile"""
+        """
+        Create a new user profile
+        """
         if not email:
             raise ValueError('User must have an email address')
         
@@ -19,7 +22,9 @@ class UserProfileManager(BaseUserManager):
         return user
     
     def create_superuser(self,email,name,password):
-        """Create and save a new superuser with given details"""
+        """
+        Create and save a new superuser with given details
+        """
         user = self.create_user(email,name,password)
 
         user.is_superuser = True
@@ -30,7 +35,9 @@ class UserProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
-    """Database model for user in the system"""
+    """
+    Database model for user in the system
+    """
 
     email = models.EmailField(max_length=255,unique=True)
     name = models.CharField(max_length=255)
@@ -43,13 +50,36 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve full name of user"""
+        """
+        Retrieve full name of user
+        """
         return self.name
     
     def get_short_name(self):
-        """Retrieve shot name of user"""
+        """
+        Retrieve shot name of user
+        """
         return self.name
     
     def __str__(self):
-        """Return string representation of our user"""
+        """
+        Return string representation of our user
+        """
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """
+    Profile status update
+    """
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        Return the model as a string
+        """
+        return self.status_text
--- profiles_project/profiles_api/serializers.py
+++ profiles_project/profiles_api/serializers.py
@@ -4,13 +4,15 @@ from profiles_api import models
 
 
 class HelloSerializer(serializers.Serializer):
-    """Serializers a name filed for testing for APIView"""
-
+    """
+    Serializers a name filed for testing for APIView
+    """
     name = serializers.CharField(max_length=10)
     
 class UserProfileSerializer(serializers.ModelSerializer):
-    """Serializers a user profile object"""
-
+    """
+    Serializers a user profile object
+    """
     class Meta:
         model = models.UserProfile
         fields = ('id','email','name','password')
@@ -22,7 +24,9 @@ class UserProfileSerializer(serializers.ModelSerializer):
         }
     
     def create(self, validated_data):
-        """Create and return a new user"""
+        """
+        Create and return a new user
+        """
         user = models.UserProfile.objects.create_user(
             email = validated_data['email'],
             name = validated_data['name'],
@@ -30,3 +34,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+    
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer profile feed items
+    """
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id','user_profile','status_text','created_on')
+        extra_kwargs = {'user_profile':{'read_only':True}}
